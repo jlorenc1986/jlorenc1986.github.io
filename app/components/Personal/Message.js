aee305cf7f8647297a1d34253b2750bec723d51a
Lorenc Estrefi <jlorenc1986@gmail.com>
2015-12-01 19:55:30 +0100

    adds correct fonts

--- app/components/Personal/Message.js
+++ app/components/Personal/Message.js
@@ -5,7 +5,9 @@ export default class Message extends Component{
   render() {
     return (
       <div className='pure-g'>
-        <div className='pure-u-1-1' > Message</div>
+        <div className='pure-u-1-1' > "Hi Everyone, my name is Lorenc :)
+
+         I m a  passionate Senior Frontend Engineer"</div>
 
     </div >
     );
