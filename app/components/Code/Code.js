2cc65dac3cb502d24bc5eb27ce138117aa479950
Lorenc Estrefi <jlorenc1986@gmail.com>
2015-12-02 23:29:00 +0100

    wip refactoring

--- app/components/Code/Code.js
+++ app/components/Code/Code.js
@@ -3,9 +3,9 @@ import React, { PropTypes } from 'react'
 const Code = React.createClass({
   render () {
     return (
-      <div className='pure-u-1-1' >
-<ul className="fa-ul">
-<li>       <i className="fa fa-spin fa-bitbucket fa-lg "></i> Bitbucket </li>
+      <div className='pure-u-1-1 utils-top-margin-5 list-social'>
+<ul>
+<li>       <i className="fa fa-spin fa-bitbucket fa-lg " ></i><a href='http://bitbucket.org/jlorenc1986/'> Bitbucket</a> </li>
     <li>   <i className="fa fa-github-alt fa-rotate-90"></i> Github</li>
       <li> <i className="fa fa-xing"></i>Xing</li>
        <li><i className="fa  fa-skype"></i>Skype</li>
--- app/components/Personal/Message.js
+++ app/components/Personal/Message.js
@@ -5,11 +5,11 @@ export default class Message extends Component{
   render() {
     return (
 
-        <div className='pure-u-1-1' > "Hi Everyone, my name is Lorenc :)
+        <div className='pure-u-1-1 utils-top-margin-5' > "Hi Everyone, my name is Lorenc :)
 
          I m a  passionate Senior Frontend Engineer"</div>
 
-  
+
     );
   }
 }
