07add0a709f6ba6e28ae1ea4982949b0262095cf
Lorenc Estrefi <jlorenc1986@gmail.com>
2015-12-03 17:57:42 +0100

    fixs url

--- app/components/Code/Code.js
+++ app/components/Code/Code.js
@@ -6,13 +6,11 @@ const Code = React.createClass({
       <div className='pure-u-1-1 list-social utils-top-margin-5'>
 <ul>
 <li>       <i className="fa fa-spin fa-bitbucket fa-lg " ></i><a href='http://bitbucket.org/jlorenc1986/'> Bitbucket</a> </li>
-    <li>   <i className="fa fa-github-alt fa-rotate-90"></i><a href='http://bitbucket.org/jlorenc1986/'> Github </a></li>
-      <li> <i className="fa fa-xing"></i><a href='http://bitbucket.org/jlorenc1986/'>Xing </a></li>
-       <li><i className="fa  fa-skype"></i><a href='http://bitbucket.org/jlorenc1986/'>Skype </a></li>
-       <li><i className="fa  fa-twitter-square fa-spin"></i><a href='http://bitbucket.org/jlorenc1986/'>Twitter </a></li>
-       <li><i className="fa  fa-google-plus"></i><a href='http://bitbucket.org/jlorenc1986/'>Google+ </a></li>
-       <li><i className="fa  fa-spotify"></i><a href='http://bitbucket.org/jlorenc1986/'>Spotify</a></li>
-       <li><i className="fa  fa-slack fa-pulse"></i><a href='http://bitbucket.org/jlorenc1986/'>Slack</a></li>
+    <li>   <i className="fa fa-github-alt fa-rotate-90"></i><a href='https://github.com/jlorenc1986'> Github </a></li>
+      <li> <i className="fa fa-xing"></i><a href='https://www.xing.com/profile/Lorenc_Estrefi'>Xing </a></li>
+       <li><i className="fa  fa-envelope fa-spin"></i><a href='mailto:jlorenc1986@gmail.com'>Mail </a></li>
+       <li><i className="fa  fa-twitter-square fa-spin"></i><a href='https://twitter.com/jslorenc'>Twitter </a></li>
+       <li><i className="fa  fa-google-plus"></i><a href='https://plus.google.com/102563518525693610003/about'>Google+ </a></li>
 </ul>
       </div>
     )
