f547e01e724146594b28ec42f676dcc8d4a17f2c
Lorenc Estrefi <jlorenc1986@gmail.com>
2015-12-04 19:21:16 +0100

    adds conf for deploy

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -4,16 +4,15 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './app/app.js'
-  },
+  entry: [
+    './app/app'
+  ],
   output: {
     filename: '[name].min.js',
-    path: path.join(__dirname, 'dist'),
-    publicPath: ''
+    path: path.join(__dirname, 'dist')
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
@@ -22,25 +21,32 @@ module.exports = {
       '__DEVTOOLS__': false
     }),
     new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    }),
-    new ExtractTextPlugin('app.css', { allChunks: true }),
+     compressor: {
+       warnings: false
+     }
+   }),
+   new ExtractTextPlugin('app.scss', { allChunks: true }),
     new HtmlWebpackPlugin({
-      title: 'jlorenc1986 Lorenc Estrefi',
+      title: 'jlorenc1986',
       filename: 'index.html',
       template: 'index.template.html',
-      favicon: path.join(__dirname, 'assets/images/favicon.ico')
+      favicon: path.join(__dirname, 'assets', 'images', 'favicon.ico')
     })
   ],
   module: {
     loaders: [
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!cssnext-loader') },
-      { test: /\.js$/, loaders: ['babel'], include: path.join(__dirname, 'lib') }
+      { test: /\.css$/, loader: 'style-loader!css-loader!cssnext-loader' },
+
+    {
+        test: /\.(jpg|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+    },
+
+      { test: /\.scss$/, loaders:["style","css","sass"] },
+      { test: /\.js$/, exclude: /node_modules/ , loaders: ['react-hot', 'babel'] }
     ]
-  },
-  cssnext: {
-    browsers: 'last 2 versions'
   }
 };
