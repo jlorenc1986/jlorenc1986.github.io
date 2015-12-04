963e1c02bf6b397060f30e5ffe268f7ac95e8394
Lorenc Estrefi <jlorenc1986@gmail.com>
2015-12-04 20:56:07 +0100

    adds images

--- app/components/Personal/Gravatar.js
+++ app/components/Personal/Gravatar.js
@@ -8,7 +8,7 @@ export default class Gravatar extends Component{
     return (
 
         <div className='pure-u-1 pure-u-md-1-2' >
-          <img className='pure-img jsl-avatar' src='../images/lorenc.jpg'/>
+          <img className='pure-img jsl-avatar' src='lorenc.jpg'/>
          </div>
 
 
